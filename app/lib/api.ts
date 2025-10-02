b0df27ff0c5e113fcae3fd27f4fca2aa11590ca6
Fikr Lazuardi <44257474+fik-r@users.noreply.github.com>
2025-10-02 11:50:25 +0700

    fix build

--- app/lib/api.ts
+++ app/lib/api.ts
@@ -37,51 +37,13 @@ api.interceptors.request.use((request) => {
     });
 });
 
-// Request interceptor to automatically add auth token
-// api.interceptors.request.use(
-//     async (config) => {
-//         try {
-//             console.log(config.headers)
-//             // For server-side requests, get session from cookies
-//             if (typeof window === "undefined" && config.headers?.getCookie) {
-//                 const cookieHeader = config.headers.getCookie as string;
-//                 console.log("token")
-//                 if (cookieHeader) {
-//                     const session = await getSession(cookieHeader);
-//                     const token = session.get("access_token");
-//                     if (token) {
-//                         config.headers.Authorization = `Bearer ${token}`;
-//                         console.log("token", token)
-//                     }
-//                 }
-//             }
-//             // For client-side, token will be attached via cookie automatically
-//             return config;
-//         } catch (error) {
-//             return Promise.reject(error);
-//         }
-//     },
-//     (error) => Promise.reject(error)
-// );
-
-// api.interceptors.request.use((request) => {
-//     // write down your request intercept.
-//     return AxiosLogger.requestLogger(request, {
-//         prefixText: 'your prefix',
-//         dateFormat: 'HH:MM:ss',
-//         headers: true,
-//     });
-// });
-// api.interceptors.response.use(AxiosLogger.responseLogger, AxiosLogger.errorLogger);
-
-// Response interceptor for error handling
 api.interceptors.response.use(
     (response) => response,
     (error: AxiosError) => {
         const customError: ApiError = {
             status: error.response?.status || 500,
             message: (error.response?.data as any)?.message || error.message,
-            data: error.response?.data,
+            data: error.response?.data as any,
         };
         return Promise.reject(customError);
     }
